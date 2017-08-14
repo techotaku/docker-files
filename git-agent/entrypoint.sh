977366c0ecf49df56b1fd83405659495b6ede3cb
Ian Li <techotaku@users.noreply.github.com>
2017-08-14 16:46:48 +0800

    Add git-agent.

---
+++ git-agent/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "machine $GIT_HOST" > ~/.netrc
+echo "login $GIT_USERNAME" >> ~/.netrc
+echo "login `cat /run/secrets/git-pwd`" >> ~/.netrc
+chmod 600 ~/.netrc
+
+if [ ! -d "$GIT_PATH/.git" ]; then
+    echo "Git repository doesn't exist, will clone to local."
+    git clone $GIT_URL $GIT_PATH
+else
+    echo "Git repository exists, will check synchronization status."
+fi
+
+cd $GIT_PATH
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Already up to date."
+elif [ $LOCAL = $BASE ]; then
+    echo "Pulling..."
+    git pull
+fi
+
+cd $OLDPWD
